eb05c6deb6268b13ae3e33aab44ddc07e579ae78
phubao1999 <phutuongbao1999@gmail.com>
2019-05-07 23:24:51 +0700

    Components

---
+++ ES6/ReactJs/setup/todo-list/src/components/TodoItem.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class TodoItem extends Component {
+    render(){
+        return (
+            <div className="TodoItem">
+                <p>Đi Chợ</p>
+            </div>
+        );
+    }
+}
+
+export default TodoItem;
